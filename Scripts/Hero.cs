f0d096fac18a2e89eb625a189ddd5146622295ab
Arnaud Drain <arnaud.drain@phantombuster.com>
2021-10-02 13:32:20 +0200

    add basic hero

---
+++ Scripts/Hero.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public class Hero : KinematicBody2D
+{
+  [Export] public int speed = 200;
+
+  public Vector2 velocity = new Vector2();
+
+  public void GetInput()
+  {
+    velocity = new Vector2();
+
+    if (Input.IsActionPressed("ui_right"))
+      velocity.x += 1;
+
+    if (Input.IsActionPressed("ui_left"))
+      velocity.x -= 1;
+
+    if (Input.IsActionPressed("ui_down"))
+      velocity.y += 1;
+
+    if (Input.IsActionPressed("ui_up"))
+      velocity.y -= 1;
+
+    velocity = velocity.Normalized() * speed;
+  }
+
+  public override void _PhysicsProcess(float delta)
+  {
+    GetInput();
+    velocity = MoveAndSlide(velocity);
+  }
+}
