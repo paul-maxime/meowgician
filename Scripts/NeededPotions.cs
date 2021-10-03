28eb98de7ef68e536de1bef6988b4ee6e7af9852
Selimabbas <selim.abbassaid@gmail.com>
2021-10-03 18:18:58 +0200

    Add plus sign to potions in cauldron

--- Scripts/NeededPotions.cs
+++ Scripts/NeededPotions.cs
@@ -4,19 +4,29 @@ using System;
 public class NeededPotions : Node2D
 {
 	private PackedScene smallPotion = ResourceLoader.Load<PackedScene>("res://Scenes/SmallPotion.tscn");
-	private SmallPotion potion1;
-	private SmallPotion potion2;
+	private PackedScene mathOperator = ResourceLoader.Load<PackedScene>("res://Scenes/MathOperator.tscn");
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+		uint numberOfPotions = 4;
 		Vector2 cauldronSize = GetNode<Sprite>("/root/Root/Cauldron/Sprite").Texture.GetSize();
-		potion1 = (SmallPotion) smallPotion.Instance();
-		potion1.init(- (cauldronSize.x / 8) + 4, 0f, Godot.GD.Randi() % 4);
-		potion2 = (SmallPotion) smallPotion.Instance();
-		potion2.init(cauldronSize.x / 8 - 4, 0f, Godot.GD.Randi() % 4);
-		AddChild(potion1);
-		AddChild(potion2);
+		float minX = - (cauldronSize.x / 4f) + 4;
+		float maxX = cauldronSize.x / 4f - 4;
+
+		for (int i = 0; i < numberOfPotions; i++) 
+		{
+			if (i != 0)
+			{
+				MathOperator plusSign = mathOperator.Instance<MathOperator>();
+				plusSign.Position = new Vector2(minX + i * 7, 0);
+				AddChild(plusSign);
+			}
+
+			SmallPotion potion = (SmallPotion) smallPotion.Instance();
+			potion.init(minX + i * 7 + 3, 0f, Godot.GD.Randi() % 4);
+			AddChild(potion);
+		}
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
