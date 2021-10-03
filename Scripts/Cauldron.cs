2a6f72126014e215f0637a6a791f3f4412fbd9e1
Maxou <github@paulmaxime.fr>
2021-10-04 00:30:24 +0200

    The cauldron panics a bit over time

--- Scripts/Cauldron.cs
+++ Scripts/Cauldron.cs
@@ -1,14 +1,21 @@
 using Godot;
 using Godot.Collections;
+using System;
 
 public class Cauldron : StaticBody2D
 {
+	public float Instability { get => instability; set => SetInstability(value); }
+	private float instability;
+
 	private PackedScene smallPotion = ResourceLoader.Load<PackedScene>("res://Scenes/SmallPotion.tscn");
 	private PackedScene mathOperator = ResourceLoader.Load<PackedScene>("res://Scenes/MathOperator.tscn");
 	private Array<SmallPotion> neededPotions = new Array<SmallPotion> { };
 	private uint numberOfPotions = 4;
 	private float minX;
 	private float maxX;
+
+	private CPUParticles2D particles;
+	private Sprite sprite;
 
 	public void TryToDrop(Array<Potion> potions)
 	{
@@ -42,8 +49,12 @@ public class Cauldron : StaticBody2D
 		}
 	}
 
+
 	public override void _Ready()
 	{
+		particles = GetNode<CPUParticles2D>("CPUParticles2D");
+		sprite = GetNode<Sprite>("Sprite");
+
 		Vector2 cauldronSize = GetNode<Sprite>("Sprite").Texture.GetSize();
 		minX = -(cauldronSize.x / 4f) + 4;
 		maxX = cauldronSize.x / 4f - 4;
@@ -55,5 +66,52 @@ public class Cauldron : StaticBody2D
 			AddChild(plusSign);
 		}
 		GenerateNeededPotions();
+	}
+
+	public override void _Process(float delta)
+	{
+		base._Process(delta);
+		Instability += delta / 60f;
+	}
+
+	private void SetInstability(float value)
+	{
+		float previousLevel = instability;
+		instability = value;
+
+		particles.Lifetime = Mathf.Lerp(1.5f, 0.2f, instability);
+		particles.InitialVelocity = Mathf.Lerp(3.5f, 135f, instability);
+		particles.ScaleAmount = Mathf.Lerp(0.2f, 0.5f, instability);
+
+		if (Math.Floor(previousLevel * 100f) != Math.Floor(instability * 100f))
+		{
+			sprite.Modulate = ModulatedColor;
+
+			Color color = ParticlesColor;
+			particles.ColorRamp.SetColor(1, color);
+			particles.ColorRamp.SetColor(2, color);
+		}
+	}
+
+	public Color ParticlesColor
+	{
+		get
+		{
+			float r = Mathf.Lerp(0x6d / 255f, 0xd0 / 255f, instability);
+			float g = Mathf.Lerp(0xaa / 255f, 0x46 / 255f, instability);
+			float b = Mathf.Lerp(0x2c / 255f, 0x48 / 255f, instability);
+			return new Color(r, g, b);
+		}
+	}
+
+	public Color ModulatedColor
+	{
+		get
+		{
+			float r = Mathf.Lerp(1f, 0xd0 / 255f, instability);
+			float g = Mathf.Lerp(1f, 0x46 / 255f, instability);
+			float b = Mathf.Lerp(1f, 0x48 / 255f, instability);
+			return new Color(r, g, b);
+		}
 	}
 }
