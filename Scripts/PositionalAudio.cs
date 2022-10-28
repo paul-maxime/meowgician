2cd65a76154c18bbe0008b4c2fb2efedd8b1c649
Maxou <github@paulmaxime.fr>
2022-10-28 05:04:09 +0200

    Update imports, fix the positional audio

--- Scripts/PositionalAudio.cs
+++ Scripts/PositionalAudio.cs
@@ -4,15 +4,17 @@ public class PositionalAudio : AudioStreamPlayer2D
 {
 	private Node2D source;
 	private Node2D listener;
+	private Node2D camera;
 
 	public override void _Ready()
 	{
 		source = GetParent<Node2D>();
 		listener = GetNode<Node2D>("/root/Game/Hero");
+		camera = GetNode<Node2D>("/root/Game/Camera2D");
 	}
 
 	public override void _Process(float delta)
 	{
-		GlobalPosition = (source.Position - listener.Position) + GetViewport().Size / 2;
+		GlobalPosition = (source.Position - listener.Position) + camera.Position;
 	}
 }
